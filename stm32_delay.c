0897621620a7b61570d140555bda3587d84ac9fb
Vinícius Müller Silveira <vini.s.muller@gmail.com>
2018-05-24 19:16:55 -0300

    Fixed SysTick_Handler

--- stm32_delay.c
+++ stm32_delay.c
@@ -1,13 +1,21 @@
-#include <delay.h>
+#include <stm32_delay.h>
 
 
 // SysTick interrupt handler
-void SysTick_Handler() {
-	if (TimingDelay != 0) { TimingDelay--; }
+void SysTick_Handler(void)
+{
+	if (TimingDelay != 0)
+	{
+		TimingDelay--;
+	}else
+	{
+		SysTick->CTRL	= 0;
+	}
 }
 
 // Do delay for mSecs milliseconds
 void Delay_ms(uint32_t mSecs) {
+	SystemCoreClockUpdate();
 	SysTick_Config(SystemCoreClock / DELAY_TICK_FREQUENCY_MS);
 	TimingDelay = mSecs+1;
 	while (TimingDelay != 0);
@@ -15,6 +23,7 @@ void Delay_ms(uint32_t mSecs) {
 
 // Do delay for nSecs microseconds
 void Delay_us(uint32_t uSecs) {
+	SystemCoreClockUpdate();
 	SysTick_Config(SystemCoreClock / DELAY_TICK_FREQUENCY_US);
 	TimingDelay = uSecs+1;
 	while (TimingDelay != 0);
--- stm32_delay.h
+++ stm32_delay.h
@@ -1,7 +1,7 @@
 #include "stm32l0xx.h"
 
 #define DELAY_TICK_FREQUENCY_US 1000000   /* = 1MHZ -> microseconds delay */
-#define DELAY_TICK_FREQUENCY_MS 1000      /* = 1kHZ -> milliseconds delay */
+#define DELAY_TICK_FREQUENCY_MS 1000   	  /* = 1kHZ -> milliseconds delay */
 
 static __IO uint32_t TimingDelay; // __IO -- volatile
 
@@ -9,5 +9,5 @@ static __IO uint32_t TimingDelay; // __IO -- volatile
 /*
  *   Declare Functions
  */
-extern void Delay_ms(uint32_t nTime);
-extern void Delay_us(uint32_t nTime);
+extern void Delay_ms(uint32_t mSecs);
+extern void Delay_us(uint32_t uSecs);
