282cf4043bba7182b15f69b5a188735efb47ac57
Vinícius Müller Silveira <vini.s.muller@gmail.com>
2018-05-30 10:52:37 -0300

    Delay with timer6. Min delay 5us

--- stm32_delay.c
+++ stm32_delay.c
@@ -1,30 +1,44 @@
 #include <stm32_delay.h>
 
+uint16_t pscMs;
+uint16_t pscUs;
 
-// SysTick interrupt handler
-void SysTick_Handler(void)
+
+void delayInit(void)
 {
-	if (TimingDelay != 0)
-	{
-		TimingDelay--;
-	}else
-	{
-		SysTick->CTRL	= 0;
-	}
+	//Update SystemCoreClock var with clock value
+	SystemCoreClockUpdate();
+
+	//Timer configuration.
+	RCC->APB1ENR	|= RCC_APB1ENR_TIM6EN; //Enable clock timer
+
+	TIMER->CR1		= TIM_CR1_OPM | TIM_CR1_URS | TIM_CR1_ARPE;
+	TIMER->CR2		= 0;
+
+	pscMs	= (SystemCoreClock/DELAY_TICK_FREQUENCY_MS /MULTIPLIER_FACTOR) - 1;
+	pscUs	= (SystemCoreClock/DELAY_TICK_FREQUENCY_US /MULTIPLIER_FACTOR) - 1;
 }
 
 // Do delay for mSecs milliseconds
-void Delay_ms(uint32_t mSecs) {
-	SystemCoreClockUpdate();
-	SysTick_Config(SystemCoreClock / DELAY_TICK_FREQUENCY_MS);
-	TimingDelay = mSecs+1;
-	while (TimingDelay != 0);
+void Delay_ms(uint32_t mSecs)
+{
+	TIMER->SR 		&= ~TIM_SR_UIF;
+	TIMER->PSC		= pscMs;
+	TIMER->ARR		= mSecs * MULTIPLIER_FACTOR;
+
+	TIMER->EGR		|= TIM_EGR_UG;
+	TIMER->CR1		|= TIM_CR1_CEN;
+	while (!(TIMER->SR & TIM_SR_UIF));
 }
 
 // Do delay for nSecs microseconds
-void Delay_us(uint32_t uSecs) {
-	SystemCoreClockUpdate();
-	SysTick_Config(SystemCoreClock / DELAY_TICK_FREQUENCY_US);
-	TimingDelay = uSecs+1;
-	while (TimingDelay != 0);
+void Delay_us(uint32_t uSecs)
+{
+	TIMER->SR 		&= ~TIM_SR_UIF;
+	TIMER->PSC		= pscUs;
+	TIMER->ARR		= uSecs * MULTIPLIER_FACTOR;
+
+	TIMER->EGR		|= TIM_EGR_UG;
+	TIMER->CR1		|= TIM_CR1_CEN;
+	while (!(TIMER->SR & TIM_SR_UIF));
 }
--- stm32_delay.h
+++ stm32_delay.h
@@ -2,12 +2,11 @@
 
 #define DELAY_TICK_FREQUENCY_US 1000000   /* = 1MHZ -> microseconds delay */
 #define DELAY_TICK_FREQUENCY_MS 1000   	  /* = 1kHZ -> milliseconds delay */
-
-static __IO uint32_t TimingDelay; // __IO -- volatile
-
-
+#define TIMER					TIM6
+#define MULTIPLIER_FACTOR		16
 /*
  *   Declare Functions
  */
 extern void Delay_ms(uint32_t mSecs);
 extern void Delay_us(uint32_t uSecs);
+extern void delayInit(void);
