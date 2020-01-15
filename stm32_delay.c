0182f1b4cbe2fbccaec60df93f42c742e97009e2
Vini <vini.s.muller@gmail.com>
2020-01-15 12:59:00 -0300

    working with systick and documented

--- stm32_delay.c
+++ stm32_delay.c
@@ -1,30 +1,55 @@
 #include "stm32_delay.h"
 
+static uint8_t dF = 1;
+static int32_t ticks = 0;
+
+//Init SysTick
 void delayInit(void)
 {
+	//Updates SystemCoreClock var
+	//If using HSE, be sure that the function is returning the right value
 	SystemCoreClockUpdate();
-	SysTick->LOAD	= (SystemCoreClock / DELAY_TICK_FREQUENCY_US) - 1;
+
+	//In order to improve accuracy, when using a system clock lower than 48MHz,
+	//we will use MIN_TICKS as a time base. This means that one tick is equal
+	//to dF microseconds. When system clock is greater or equal to 48MHz, one
+	//tick equals to 1 microsecond.
+	if(SystemCoreClock >= 48000000)
+	{
+		SysTick->LOAD	= (SystemCoreClock / DELAY_TICK_US) - 1;
+		dF = 1;
+	}
+	else
+	{
+		SysTick->LOAD	= MIN_TICKS - 1; //Minimum value
+		dF = MIN_TICKS / (SystemCoreClock / DELAY_TICK_US);
+	}
 	SysTick->VAL	= 0;
+	//SysTick clock source is SYSCLK
 	SysTick->CTRL	= SysTick_CTRL_CLKSOURCE_Msk |
 			SysTick_CTRL_ENABLE_Msk;
 }
 
 // Do delay for mSecs milliseconds
-void Delay_ms(uint32_t mSecs)
+// max delay is 65535mS
+void Delay_ms(const uint16_t mSecs)
 {
-	while(mSecs)
+	ticks *= 1000 / dF; //Calculates the number of ticks
+	while(ticks)
 	{
-		Delay_us(1000);
-		mSecs--;
+		while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+		ticks--;
 	}
 }
 
-// Do delay for nSecs microseconds
-void Delay_us(uint32_t uSecs)
+// Do delay for uSecs microseconds
+// max delay is 65535uS
+void Delay_us(const uint16_t uSecs)
 {
-	while(uSecs)
+	ticks = uSecs / dF;//Calculates the number of ticks
+	while(ticks)
 	{
 		while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
-		uSecs--;
+		ticks--;
 	}
 }
